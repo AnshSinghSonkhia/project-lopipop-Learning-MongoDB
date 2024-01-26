138b5484dc1234b6622c49536f6c4afa250d23b6
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2024-01-26 17:56:40 +0530

    Connected MongoDB & Mongoose
    Connected MongoDB & Mongoose, adding more EJS Code

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,8 @@ router.get("/create", async function (req, res) {
     age: 20,
     name: "Ansh Singh Sonkhia",
   });
-  res.send("Successfullllly Created");
+  //res.send("Successfullllly Created");
+  //console.log(createduser);
   res.send(createduser);
   // This "userModel.create" is asynchronoud JS, So, will go into the side stack & will be implemented after the completion of main stack Synchronous code. That's why we use "await" before it.
   // nhi toh user ko pehle hi message chala jayega
@@ -27,6 +28,7 @@ router.get("/create", async function (req, res) {
 // CRUD - Read
 
 router.get("/allusers", async function(req,res){
+  // let allusers = await userModel.find();
   let allusers = await userModel.findOne({username: "harshita"});
   res.send(allusers);
 });
